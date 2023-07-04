069666cb221a1de824ca7686ac1f5551d4f96d5e
Josh-Beck <joshuathomasbeck@gmail.com>
2023-07-04 17:02:20 -0400

    small sizing change

--- src/components/RegularTextBar.js
+++ src/components/RegularTextBar.js
@@ -2,12 +2,12 @@ import * as React from "react"
 
 export default function RegularTextBar(props) {
   return <div>
-            <div className="items-center hidden xl:block mt-24">
+            <div className="items-center hidden md:block mt-24">
                 <h1 className="items-center ">{props.title}</h1>
                 <h2 className="items-center text-center pb-10 pt-3 ">{props.subTitle}</h2>
                 <div className="paragraph-text">{props.content}</div>
             </div>
-            <div className="mt-20 xl:hidden">
+            <div className="mt-20 md:hidden">
                 <h1 className="items-center px-1">{props.title}</h1>
                 <h2 className="items-center text-center pb-10 pt-3 px-3">{props.subTitle}</h2>
                 <div className="text-center paragraph-color-text px-3">{props.content}</div>
