bf90aa39be67d9fb69a36d5effae495edf09674b
josh-beck <joshuathomasbeck@gmail.com>
2023-07-11 22:25:09 -0400

    small change

--- src/components/CardContent.js
+++ src/components/CardContent.js
@@ -5,15 +5,12 @@ export default function CardContent(props) {
                 <div id={props.cardTitle} className="text-2xl primary-text">
                     {props.cardTitle}
                 </div>
-
                 <div className="text-2xl font-bold pt-1 paragraph-color-text">
                     {props.cardSubTitle}
                 </div>
-
                 <div className="text-lg font-bold pt-1 paragraph-color-text">
                     {props.cardDate}
                 </div>
-
                 <div className="text-xl pt-3 paragraph-text">
                     {props.cardData}
                 </div>     
