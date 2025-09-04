461832fb8593ca524b26b1729d0da5450e030562
Joshua Beck <joshuathomasbeck@gmail.com>
2025-09-04 08:46:57 -0400

    color changes

--- src/components/SideBarLayout.js
+++ src/components/SideBarLayout.js
@@ -38,7 +38,7 @@ export default function SideBarLayout(props) {
                 </div>
                 {/* Scroll content */}
                 <div className="w-full z-10 min-w-0 overflow-auto ...">
-                    <div className="ml-[43%] mr-[8%]">
+                    <div className="ml-[43%] mr-[8%] pb-[5%]">
                         {props.children}
                     </div>
                     
